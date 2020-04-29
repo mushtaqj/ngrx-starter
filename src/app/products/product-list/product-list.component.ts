e4ccb01fccf368229d95d1863ec1b3d0a6e8bafd
Mushtaq Jameel <Mushtaq.Jameel@cambio.lk>
2020-04-30 02:16:08 +0530

    Added selector to product-list.component.ts to fetch showProductCode

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -38,11 +38,9 @@ export class ProductListComponent implements OnInit, OnDestroy {
       error: (err: any) => this.errorMessage = err.error
     });
 
-    this.store.pipe(select('products')).subscribe(products => {
-      if (products) {
-        this.displayCode = products.showProductCode;
-      }
-    });
+    this.store.pipe(select(fromProduct.getShowProductCode)).subscribe(showProductCode =>
+      this.displayCode = showProductCode
+    );
   }
 
   ngOnDestroy(): void {
