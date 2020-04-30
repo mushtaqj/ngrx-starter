720afae3cd22df9483c796f977847dd0b1dedc9d
Mushtaq Jameel <Mushtaq.Jameel@cambio.lk>
2020-05-01 00:12:46 +0530

    #feat - Adds typed actions for Product updates

--- src/app/products/state/product.actions.ts
+++ src/app/products/state/product.actions.ts
@@ -9,7 +9,10 @@ export enum ProductActionTypes {
   InitializeCurrentProduct = '[Product] Initialize Current Product',
   Load = '[Product] Load',
   LoadSuccess = '[Product] Load Success',
-  LoadFail = '[Product] Load Fail'
+  LoadFail = '[Product] Load Fail',
+  UpdateProduct = '[Product] Update Product',
+  UpdateProductSuccess = '[Product] Update Product Success',
+  UpdateProductFail = '[Product] Update Product Fail'
 }
 
 export class ToggleProductCode implements Action {
@@ -48,6 +51,24 @@ export class LoadFail implements Action {
   constructor(public payload: string) {}
 }
 
+export class UpdateProduct implements Action {
+  readonly type = ProductActionTypes.UpdateProduct;
+
+  constructor(public payload: Product) {}
+}
+
+export class UpdateProductSuccess implements Action {
+  readonly type = ProductActionTypes.UpdateProductSuccess;
+
+  constructor(public payload: Product) {}
+}
+
+export class UpdateProductFail implements Action {
+  readonly type = ProductActionTypes.UpdateProductFail;
+
+  constructor(public payload: string) {}
+}
+
 export type ProductActions = ToggleProductCode
   | SetCurrentProduct
   | ClearCurrentProduct
@@ -55,3 +76,6 @@ export type ProductActions = ToggleProductCode
   | Load
   | LoadSuccess
   | LoadFail
+  | UpdateProduct
+  | UpdateProductSuccess
+  | UpdateProductFail
